6592bf75abc24da8b46ff7b5ee45e22db220c311
Paweł Rybka <stasiek.rybka@gmail.com>
2018-01-07 12:04:46 +0100

    krok piaty 1

--- game.js
+++ game.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded',function(){
         'playerB': 'blue'
     };
     var currentPlayer;
+    var emptyFields;
 
 initGame();
 function initGame(){
    
     var fields = document.querySelectorAll('.board > div');
     currentPlayer = 'playerA';
+    emptyFields = 9;
    
     fields.forEach(field => field.addEventListener('click',filedClickHandler));
    
@@ -18,9 +20,12 @@ function initGame(){
 function filedClickHandler(){
    var playerClass = playerClasses[currentPlayer];
    this.classList.add(playerClass);
+   emptyFields--;
    currentPlayer =currentPlayer === 'playerA' ? 'playerB': 'playerA';
    this.removeEventListener('click',filedClickHandler);
-   
+   if (emptyFields === 0){
+       alert('Koniec Gry!');
+   }
    
 }
 
