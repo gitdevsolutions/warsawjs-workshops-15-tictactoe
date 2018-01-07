fd7051aa693fde0cc952ca998aed89ee966787fd
Paweł Rybka <stasiek.rybka@gmail.com>
2018-01-07 16:05:03 +0100

    wyniki

--- game.js
+++ game.js
@@ -1,84 +1,123 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var playerClasses = {
-    playerA: "red",
-    playerB: "blue"
-  };
-  var currentPlayer;
-  var emptyFields;
+document.addEventListener('DOMContentLoaded', function() {
+	var resetButton = document.getElementById('reset-score');
+    var playerClasses = {
+        'playerA': 'red', 
+        'playerB': 'blue'
+    };
 
-  initGame();
+    var scores = {
+		'playerA': 0,
+		'playerB': 0
+	}
 
-  function initGame() {
-    var fields = document.querySelectorAll(".board > div");
+    var currentPlayer;
+	var emptyFields;
 
-    currentPlayer = "playerA";
-    emptyFields = 9;
-    fields.forEach(field => field.addEventListener("click", fieldClickHandler));
-    fields.forEach(field => field.removeAttribute('class'));
-  }
+    initGame();
 
-  function fieldClickHandler() {
-    var playerClass = playerClasses[currentPlayer];
-    this.classList.add(playerClass);
+    resetButton.addEventListener('click', function() {
+    	scores['playerA'] = 0;
+    	scores['playerB'] = 0;
 
-    emptyFields--;
-    currentPlayer = currentPlayer === "playerA" ? "playerB" : "playerA";
+		displayPlayerScore('playerA');
+		displayPlayerScore('playerB');
+	});
 
-    this.removeEventListener("click", fieldClickHandler);
+	function displayPlayerScore(player) {
+		var score = document.getElementById(`${player}-score`);
 
-    checkWinner();
-  }
+		score.innerHTML = `${player} score: ${scores[player]}`;
+	}
 
-  function checkWinner() {
-    var fields = document.querySelectorAll(".board > div");
+	function updatePlayerScore(player) {
+		scores[player]++;
+	}
 
-    var row1 = fields[0].className + fields[1].className + fields[2].className;
-    var row2 = fields[3].className + fields[4].className + fields[5].className;
-    var row3 = fields[6].className + fields[7].className + fields[8].className;
+    function displayRoundInformation() {
+		var round = document.getElementById('round-info');
 
-    var column1 =
-      fields[0].className + fields[3].className + fields[6].className;
-    var column2 =
-      fields[1].className + fields[4].className + fields[7].className;
-    var column3 =
-      fields[2].className + fields[5].className + fields[8].className;
-
-    var diagonal1 =
-      fields[0].className + fields[4].className + fields[8].className;
-    var diagonal2 =
-      fields[6].className + fields[4].className + fields[2].className;
-
-      var boardCheck = [
-        row1,
-        row2,
-        row3,
-        column1,
-        column2,
-        column3,
-        diagonal1,
-        diagonal2
-    ];
-    if (boardCheck.includes('redredred')) {
-        setTimeout(() => {
-            alert("Red Wins!");
-            initGame();
-        }, 100);
+		round.className = playerClasses[currentPlayer];
+		round.innerHTML = `Round for ${currentPlayer}`;
     }
 
-    if (boardCheck.includes('blueblueblue')) {
-        setTimeout(() => {
-            alert("Blue Wins!");
-            initGame();
-        }, 100);
-    }
+    function initGame() {
+		var fields = document.querySelectorAll('.board > div');
 
-    if(emptyFields === 0) {
-        setTimeout(() => {
-            alert('Tie');
-            initGame();
-        }, 100);
-    }
-}
+		currentPlayer = 'playerA';
+		emptyFields = 9;
+    	fields.forEach(field => field.addEventListener('click', fieldClickHandler));
+		fields.forEach(field => field.removeAttribute('class'));
 
-  }
-)
+		displayRoundInformation();
+		displayPlayerScore('playerA');
+		displayPlayerScore('playerB');
+	}
+
+	function fieldClickHandler () {
+        var playerClass = playerClasses[currentPlayer];
+        this.classList.add(playerClass);
+
+		emptyFields--;
+        currentPlayer = currentPlayer === 'playerA' ? 'playerB' : 'playerA';
+
+		displayRoundInformation();
+		this.removeEventListener('click', fieldClickHandler);
+
+		checkWinner();
+	}
+
+	function  checkWinner() {
+		var fields = document.querySelectorAll('.board > div');
+
+		// Horizontal winning configurations
+		var row1 = fields[0].className + fields[1].className + fields[2].className;
+		var row2 = fields[3].className + fields[4].className + fields[5].className;
+		var row3 = fields[6].className + fields[7].className + fields[8].className;
+
+		// Vertical winning configurations
+		var column1 = fields[0].className + fields[3].className + fields[6].className;
+		var column2 = fields[1].className + fields[4].className + fields[7].className;
+		var column3 = fields[2].className + fields[5].className + fields[8].className;
+
+		// Diagonal winning configurations
+		var diagonal1 = fields[0].className + fields[4].className + fields[8].className;
+		var diagonal2 = fields[6].className + fields[4].className + fields[2].className;
+
+		var boardCheck = [
+			row1,
+			row2,
+			row3,
+			column1,
+			column2,
+			column3,
+			diagonal1,
+			diagonal2
+		];
+
+		if (boardCheck.includes('redredred')) {
+			setTimeout(() => {
+				alert("Red Wins!");
+				updatePlayerScore('playerA');
+				initGame();
+			}, 100);
+			return;
+		}
+
+		if (boardCheck.includes('blueblueblue')) {
+			setTimeout(() => {
+				alert("Blue Wins!");
+				updatePlayerScore('playerB');
+				initGame();
+			}, 100);
+			return;
+		}
+
+		if(emptyFields === 0) {
+            setTimeout(() => {
+                alert('Tie');
+                initGame();
+            }, 100);
+            return;
+		}
+	}
+});
